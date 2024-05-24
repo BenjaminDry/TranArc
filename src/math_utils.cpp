cbe4f2afed99b05522390ecab9b7ab19c4458284
Benjamin Dry <benjamindry@icloud.com>
2024-05-24 23:57:46 +0800

    Remove Duplicate Concatenate Function

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -139,36 +139,6 @@ namespace MathUtils {
         return concatenated;
     }
 
-    // Function to merge a tensor object on the end of another tensor object
-    Tensor3D concatenate(const Tensor3D& input1, const Tensor3D& input2) {
-        std::array<int, 3> newDimensions = {
-            static_cast<int>(input1.dimension(0)),
-            static_cast<int>(input1.dimension(1)),
-            static_cast<int>(input1.dimension(2) + input2.dimension(2))
-        };
-        Tensor3D concatenated(newDimensions[0], newDimensions[1], newDimensions[2]);
-
-        // Copy data from input1
-        for (int i = 0; i < input1.dimension(0); ++i) {
-            for (int j = 0; j < input1.dimension(1); ++j) {
-                for (int k = 0; k < input1.dimension(2); ++k) {
-                    concatenated(i, j, k) = input1(i, j, k);
-                }
-            }
-        }
-
-        // Copy data from input2
-        for (int i = 0; i < input2.dimension(0); ++i) {
-            for (int j = 0; j < input2.dimension(1); ++j) {
-                for (int k = 0; k < input2.dimension(2); ++k) {
-                    concatenated(i, j, input1.dimension(2) + k) = input2(i, j, k);
-                }
-            }
-        }
-
-        return concatenated;
-    }
-
     // Compute categorial cross-entropy loss    
     float categorialCrossEntropyLoss(const Tensor3D& prediction, const Tensor3D& target) {
         float totalLoss = 0.0;
